d404f41cd9c86a0a42a6896991bb5bdf6cb5e30e
Salavat A <sa.amingaliev@gmail.com>
2024-08-20 23:20:40 -0500

    project created

---
+++ src/test/java/entities/CustomResponse.java
@@ -0,0 +1,9 @@
+package entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomResponse {
+}
---
+++ src/test/java/runner/CucumberAPIRunner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumberReport.html", "json:target/testReport.json"},
+        glue = "steps",
+        features = "src/test/resources/features",
+        tags = "",
+        dryRun = true
+)
+public class CucumberAPIRunner {
+}
